8acc5d8ccb21ca5355dd26d4a267cd488247917b
jeeyunryu <jeeyunryu@gmail.com>
2021-12-23 23:55:15 +0900

    function to drag for line added

--- src/canvas/Canvas.java
+++ src/canvas/Canvas.java
@@ -41,9 +41,10 @@ public class Canvas {
 				drawArea.line();
 			} else if (e.getSource() == paintBtn) {
 				drawArea.paint();
-			} else if (e.getSource() == boldBtn) {
-				drawArea.bold();
-			}
+		} 
+//				else if (e.getSource() == boldBtn) {
+//				drawArea.bold();
+//			}
 			
 		}
 	};
