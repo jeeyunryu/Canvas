265ec197afc6b81310a2f4d23154c5f611410cc7
jeeyunryu <jeeyunryu@gmail.com>
2021-12-27 21:16:03 +0900

    myCanvas-version 0.9

--- src/canvas/Canvas.java
+++ src/canvas/Canvas.java
@@ -1,27 +1,34 @@
 package canvas;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-
-/**
- * 
- * @author ·ùÁö¿¬
- *
- */
+import javax.swing.*;
 
 public class Canvas {
 	
-	JButton clearBtn, blackBtn, blueBtn, redBtn, eraseBtn, lineBtn, paintBtn,
-		boldBtn, textBtn;
+	final int SIZE = 20;
 	
+	static Container content;
+	
+	JButton clearBtn = new JButton("Clear");
+	JButton textBtn = new JButton("Text");
+	JButton saveBtn = new JButton("Save");
+	JButton dndBtn = new JButton("Drad & Drop");
+	JButton eraseBtn = new JButton("Eraser");
+	JButton paintBtn = new JButton("Paint Brush");
+	JButton hlighterBtn = new JButton("Spray");
+	JButton boldBtn = new JButton("Bold");
+	JButton lineBtn = new JButton("Line");
+	JButton polyBtn = new JButton("Polygonal Chain");
+	JButton rectBtn = new JButton("Rectangle");
+	JButton circBtn = new JButton("Circle");
+	JButton redBtn = new JButton("Red");
+	JButton blueBtn = new JButton("Blue");
+	JButton blackBtn = new JButton("Black");
+	
+	
+
 	DrawArea drawArea;
 	
 	ActionListener actionListener = new ActionListener() {
@@ -29,88 +36,157 @@ public class Canvas {
 		public void actionPerformed(ActionEvent e) {
 			
 			if (e.getSource() == clearBtn) {
+				
 				drawArea.clear();
+				
 			} else if (e.getSource() == redBtn) {
+				
 				drawArea.red();
+				
 			} else if (e.getSource() == blackBtn) {
+				
 				drawArea.black();
+				
 			} else if (e.getSource() == blueBtn) {
+				
 				drawArea.blue();
+				
 			} else if (e.getSource() == eraseBtn) {
+				
 				drawArea.erase();
+				
 			} else if (e.getSource() == lineBtn) {
-				drawArea.line();
+				
+				drawArea.addLine();
+				
 			} else if (e.getSource() == paintBtn) {
+				
 				drawArea.paint();
-		} 
-				else if (e.getSource() == boldBtn) {
+				
+			} else if (e.getSource() == boldBtn) {
+					
 				drawArea.bold();
+				
 			} else if (e.getSource() == textBtn) {
+				
 				drawArea.putText();
+				
+			} else if (e.getSource() == polyBtn) {
+				
+				drawArea.drawPoly(); 
+				
+			} else if (e.getSource() == rectBtn) {
+				
+				drawArea.drawRect();
+				
+			} else if (e.getSource() == circBtn) {
+				
+				drawArea.drawCircle();
+				
+			} else if (e.getSource() == hlighterBtn) {
+				
+				drawArea.highlight();
+				
+			} else if (e.getSource() == saveBtn) {
+				
+				drawArea.save();
+				
+			} else if (e.getSource() == dndBtn) {
+				
+				drawArea.draganddrop();
+				
 			}
 			
 		}
 
-//		private void putText() {
-//			JTextField txt = new JTextField();
-//			content.add(txt);
-//			
-//		}
 	};
 	
 	public static void main(String[] args) {
+		
 		new Canvas().show();
+		
 	}
 	
 	public void show() {
 		
 		JFrame frame = new JFrame("New Canvas");
-		Container content = frame.getContentPane();
+		content = frame.getContentPane();
 		
 		content.setLayout(new BorderLayout());
 		
 		drawArea = new DrawArea();
-		
+
 		content.add(drawArea, BorderLayout.CENTER);
+		
 		JPanel controls = new JPanel();
-		
-		clearBtn = new JButton("Clear");
-		clearBtn.addActionListener(actionListener);
-		blackBtn = new JButton("Black");
-		blackBtn.addActionListener(actionListener);
-		blueBtn = new JButton("Blue");
-		blueBtn.addActionListener(actionListener);
-		redBtn = new JButton("Red");
-		redBtn.addActionListener(actionListener);
-		eraseBtn = new JButton("Erase");
-		eraseBtn.addActionListener(actionListener);
-		lineBtn = new JButton("Line");
-		lineBtn.addActionListener(actionListener);
-		paintBtn = new JButton("Paint");
-		paintBtn.addActionListener(actionListener);
-		boldBtn = new JButton("Bold");
-		boldBtn.addActionListener(actionListener);
-		textBtn = new JButton("Text");
-		textBtn.addActionListener(actionListener);
-		
-		controls.add(textBtn);
-		controls.add(paintBtn);
-		controls.add(lineBtn);
-		controls.add(boldBtn);
-		controls.add(blackBtn);
-		controls.add(blueBtn);
-		controls.add(redBtn);
-		controls.add(eraseBtn);
-		
-		
-		controls.add(clearBtn);
-		
+		controls.setLayout(new FlowLayout());
 		
 		content.add(controls, BorderLayout.NORTH);
 		
-		frame.setSize(700, 700);
+		clearBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		boldBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		saveBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		dndBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		blackBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		blueBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		redBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		lineBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		polyBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		rectBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		circBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		eraseBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		hlighterBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		paintBtn.setFont(new Font("Arial", Font.PLAIN, SIZE));
+		
+		
+		boldBtn.addActionListener(actionListener);
+		saveBtn.addActionListener(actionListener);
+		dndBtn.addActionListener(actionListener);
+		clearBtn.addActionListener(actionListener);
+		
+		JPanel colorPanel = new JPanel(new GridLayout(3, 0));
+		blackBtn.addActionListener(actionListener);
+		blueBtn.addActionListener(actionListener);
+		redBtn.addActionListener(actionListener);
+		colorPanel.add(redBtn);
+		colorPanel.add(blackBtn);
+		colorPanel.add(blueBtn);
+	
+		JPanel shapePanel = new JPanel(new GridLayout(4, 0));
+		lineBtn.addActionListener(actionListener);
+		polyBtn.addActionListener(actionListener);
+		rectBtn.addActionListener(actionListener);
+		circBtn.addActionListener(actionListener);
+		shapePanel.add(rectBtn);
+		shapePanel.add(circBtn);
+		shapePanel.add(lineBtn);
+		shapePanel.add(polyBtn);
+		
+		JPanel toolPanel = new JPanel(new GridLayout(3, 0));
+		eraseBtn.addActionListener(actionListener);
+		hlighterBtn.addActionListener(actionListener);
+		paintBtn.addActionListener(actionListener);
+		toolPanel.add(paintBtn);
+		toolPanel.add(hlighterBtn);
+		toolPanel.add(eraseBtn);
+		
+		
+		controls.add(toolPanel);
+		controls.add(shapePanel);
+		
+		controls.add(boldBtn);
+		controls.add(colorPanel);
+		controls.add(dndBtn);
+		controls.add(saveBtn);
+		controls.add(clearBtn);
+		
+		DrawArea.myLabel = new JLabel();
+		content.add(DrawArea.myLabel, BorderLayout.SOUTH);
+
+		frame.setSize(900, 900);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		
 	}
 
 }
--- src/canvas/DrawArea.java
+++ src/canvas/DrawArea.java
@@ -1,197 +1,471 @@
 package canvas;
 
-import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.BasicStroke;
-import java.awt.Stroke;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.RenderingHints;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.awt.image.RenderedImage;
 
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.*;
 
-public class DrawArea extends JComponent{
+import java.io.*;
+import javax.imageio.ImageIO;
+
+public class DrawArea extends JComponent {
+	
+//	ButtonGroup colorGroup = new ButtonGroup();
+//	static JRadioButton redBtn = new JRadioButton();
+//	static JRadioButton blackBtn = new JRadioButton();
+//	static JRadioButton blueBtn = new JRadioButton();
+//	
+	
 	
 	private Image image;
+	
 	private Graphics2D g2;
-	private int currentX, currentY, oldX, oldY, latestX, latestY;
-	boolean pendown = true, line = false, isText = false;
+	private Graphics2D transg2;
 	
-	JButton etrBtn;
-	JTextField txtfield;
+	private Ellipse2D ellipse;
 	
-	public  ActionListener actionListener2 = new ActionListener() {
-			
-		public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == etrBtn) {
-					g2.drawString(txtfield.getText(), oldX, oldY);
-					repaint();
-				}
-		}
+	private Cursor cursor;
+	
+	static JLabel myLabel = new JLabel();
 
-	};
+	private int currentX = 0;
+	private int currentY = 0;
+	private int oldX = 0;
+	private int oldY = 0;
+	private int initX = 0;
+	private int initY = 0;
+	private int width = 100;
+	private int height = 100;
+	private int radius = 50;
+
+	private boolean pendown = false;
+	private boolean isLine = false;
+	private boolean isPoly = false; 
+	private boolean isFirstClk = true;
+	private boolean isRect = false;
+	private boolean isCircle = false; 
+	private boolean isHLighter = false;
+	private boolean isDnd = false;
 	
 	public DrawArea() {
 		
 		addMouseListener(new MouseAdapter() {
 			
-			
-
+			//1번. 마우스 프레스
 			public void mousePressed(MouseEvent e) {
 				
-				oldX = e.getX();
-				oldY = e.getY();
-				
-				if (isText) {
-					
-					
-					JFrame txtFrame = new JFrame();
-					Container content = txtFrame.getContentPane();
-					content.setLayout(new FlowLayout());
-					txtfield = new JTextField("hi~");
-					
-					etrBtn = new JButton("Enter");
-					etrBtn.addActionListener(actionListener2);
-									
-					
-					content.add(txtfield);
-					content.add(etrBtn);
-					
-					
-					txtFrame.setSize(100, 100);
-					txtFrame.setVisible(true);
-					
-					
-					
-				}
-			}
-			
-			public void mouseReleased(MouseEvent e) {
 				currentX = e.getX();
 				currentY = e.getY();
 				
-				if (line) {
-					g2.drawLine(oldX, oldY, currentX, currentY);
-					repaint();
+				if(isFirstClk) {
+					
+					if (isPoly) {
+						
+						oldX = e.getX();
+						oldY = e.getY();
+						isFirstClk = false;
+						
+					}
+					
+					else {
+						
+						initX = currentX;
+						initY = currentY;
+						isFirstClk = false;
+						
+					}
+					
 				}
 				
+				if (isPoly) {
+					
+					if (e.getClickCount() == 2) {
+						
+						e.getX();
+						e.getY();
+						g2.drawLine(oldX, oldY, currentX, currentY);
+						pendown = false;
+						isFirstClk = true;
+						
+					}
+					
+					else {
+						
+						g2.drawLine(oldX, oldY, currentX, currentY);
+						repaint();
+						oldX = currentX;
+						oldY = currentY;
+						
+					}
+					
+				}
+
+			}
+			
+			//2번. 마우스 릴리스
+			public void mouseReleased(MouseEvent e) {
 				
+				currentX = e.getX();
+				currentY = e.getY();
+				
+				if (isDnd) {
+				
+					clear();
+					g2.setPaint(Color.black);
+					g2.setStroke(new BasicStroke());
+					g2.draw(ellipse);
+					repaint();
+					
+					isFirstClk = true;
+				}
+				
+				// 선 (스케치) 그리기 
+				if (isLine ) {
+					
+					//g2.drawLine(initX, initY, currentX, currentY);
+					//repaint();
+					isFirstClk = true;
+					
+				}
+				
+				if (isRect) {
+					
+					//g2.drawRect(initX, initY, currentX-initX, currentY-initY);
+					//repaint();
+					isFirstClk = true;
+					
+				} 
+				
+				if(isCircle) {
+					
+					//g2.drawOval(initX, initY, currentX-initX, currentY-initY);
+					//repaint();
+					isFirstClk = true;
+					
+				}
+			
 			}
 			
 		});
 		
 		addMouseMotionListener(new MouseMotionAdapter() {
 			
-			public void mouseDragged(MouseEvent e) {
-				currentX = e.getX();
-				currentY = e.getY();
+			//3번. 마우스 이동
+			public void mouseMoved(MouseEvent e) {
+			
+				myLabel.setText("(" + e.getX() + ", " + e.getY() + ")");
 				
-				if (g2 != null && pendown) {
-					g2.drawLine(oldX, oldY, currentX, currentY);
-					repaint();
-					oldX = currentX;
-					oldY = currentY;
+				
+				if (isDnd) {
+					
+					if (ellipse.contains(e.getX(), e.getY())) {
+						
+						cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
+						setCursor(cursor);
+						
+					}
+					else {
+						
+						cursor = Cursor.getDefaultCursor();
+						setCursor(cursor);
+						
+					}
 				}
 				
-//				if(!pendown && line) {
-//					//clear();
-//					g2.drawLine(oldX, oldY, currentX, currentY);
-//					repaint();
-//					
-//				}
+				
 			}
 			
+			//4번. 마우스 이동 + 마우스 프레스
+			public void mouseDragged(MouseEvent e) {
+				
+				oldX = currentX;
+				oldY = currentY;
+				currentX = e.getX();
+				currentY = e.getY();
+
+				if (isDnd) {
+					
+					clear();
+					g2.setPaint(Color.black);
+					ellipse.setFrame(currentX - radius, currentY - radius, width, height);
+					g2.draw(ellipse);
+					repaint();
 			
+				}
+				
+				else if (pendown) {
+					
+					if (isHLighter) {
+						
+						transg2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.1f));
+						transg2.drawLine(oldX, oldY, currentX, currentY);
+						repaint();
+						
+					}
+					
+					else {
+						
+						g2.drawLine(oldX, oldY, currentX, currentY);
+						repaint();
+						
+					}
+					
+				}
+				
+				else if (isLine) {
+					clear();
+					g2.setPaint(Color.black);
+					g2.setStroke(new BasicStroke());
+					g2.drawLine(initX, initY, currentX, currentY);
+
+					repaint();
+			
+				}
+				
+				else if(isRect) {
+					clear();
+					g2.setPaint(Color.black);
+					g2.drawRect(initX, initY, currentX-initX, currentY-initY);
+					repaint();
+				}
+				
+				else if (isCircle) {
+					clear();
+					g2.setPaint(Color.black);
+					g2.drawOval(initX, initY, currentX-initX, currentY-initY);
+					repaint();
+				}
+		
+			}
+		
 		}) ;
-		
-		
-			
+	
 	}
 	
 	protected void paintComponent(Graphics g) {
+		
 		if (image == null) {
+			
 			image = createImage(getSize().width, getSize().height);
 			g2 = (Graphics2D) image.getGraphics();
+			transg2 = (Graphics2D) image.getGraphics();
 			
 			// 그래픽 노이즈 방지
 			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
 					RenderingHints.VALUE_ANTIALIAS_ON);
 			
 			clear();
+			
 		}
 		
 		g.drawImage(image, 0, 0, null);
+		
 	}
-	
-	
+
 	public void clear() {
+		
 		g2.setPaint(Color.white);
 		g2.fillRect(0, 0, getSize().width, getSize().height);
-		g2.setPaint(Color.black);
-		g2.setStroke(new BasicStroke());
+		
 		repaint();
-	}
-	
-	public void red() {
-		g2.setPaint(Color.red);
 		
 	}
 	
+	public void red() {
+		
+		g2.setPaint(Color.red);
+		transg2.setPaint(Color.red);
+	}
+	
 	public void black() {
+		
 		g2.setPaint(Color.black);
+		transg2.setPaint(Color.black);
 	
 	}
 	
 	public void blue() {
+		
 		g2.setPaint(Color.blue);
+		transg2.setPaint(Color.blue);
 		
 	}
 	
 	public void erase() {
-		g2.setPaint(Color.white);
-	}
 	
-	public void line() {
-		line = true;
-		pendown = false;
-		isText = false;
+		g2.setPaint(Color.white);
+		g2.setPaint(Color.white);
+	
+		isDnd = false;
+		isRect = false;
+		pendown = true; 
+		isLine = false;
+		isPoly = false;
+		isFirstClk = false;
+		isHLighter = false;
+		isCircle = false;
 		
 	}
 	
-	public void paint() {
-		pendown = true;
+	public void addLine() {
+		
+		g2.setStroke(new BasicStroke());
 		g2.setPaint(Color.black);
-		isText = false;
+		
+		isDnd = false;
+		isRect = false;
+		pendown = false; 
+		isLine = true;
+		isPoly = false;
+		isFirstClk = true;
+		isHLighter = false;
+		isCircle = false;
+		
+	}
+	
+	// 펜 속성 초기화 (검정색, 기본굵기)
+	public void paint() {
+		
+		g2.setPaint(Color.black);
+		g2.setStroke(new BasicStroke());
+	
+		isDnd = false;
+		isRect = false;
+		pendown = true; 
+		isLine = false;
+		isPoly = false;
+		isFirstClk = false;
+		isHLighter = false;
+		isCircle = false;
+		
 	}
 	
 	public void bold() {
 		
-		Stroke stroke = new BasicStroke(10);
+		g2.setStroke(new BasicStroke(10));
+		transg2.setStroke(new BasicStroke(10));
 		
-		
-		g2.setStroke(stroke);
-		
-	
 	}
 	
 	public void putText() {
- 
-		pendown = false;
-		isText = true;
-		line = false;
-
 		
+		g2.setPaint(Color.black);
+		
+		isDnd = false;
+		isRect = false;
+		pendown = false; 
+		isLine = false;
+		isPoly = false;
+		isFirstClk = false;
+		isHLighter = false;
+		isCircle = false;
 		
 	}
+	
+	public void drawPoly() {
+		
+		g2.setStroke(new BasicStroke());
+		g2.setPaint(Color.black);
+		
+		isDnd = false;
+		isRect = false;
+		pendown = false; 
+		isLine = false;
+		isPoly = true;
+		isFirstClk = true;
+		isHLighter = false;
+		isCircle = false;
+		
+	}
+	
+	// 마우스 프레스 + 마우스 이동 + 마우스 릴리스
+	public void drawRect() {
+		
+		g2.setStroke(new BasicStroke());
+		g2.setPaint(Color.black);
 
+		isDnd = false;
+		isRect = true;
+		pendown = false; 
+		isLine = false;
+		isPoly = false;
+		isFirstClk = true;
+		isHLighter = false;
+		isCircle = false;
+		
+	}
+	
+	public void drawCircle() {
+		
+		g2.setStroke(new BasicStroke());
+		g2.setPaint(Color.black);
+
+		isDnd = false;
+		isRect = false;
+		pendown = false; 
+		isLine = false;
+		isPoly = false;
+		isFirstClk = true;
+		isHLighter = false;
+		isCircle = true;
+	}
+	
+	public void highlight() {
+		
+		
+		
+		transg2.setStroke(new BasicStroke());
+		transg2.setPaint(Color.black);
+		
+		isDnd = false;
+		isRect = false;
+		pendown = true; 
+		isLine = false;
+		isPoly = false;
+		isFirstClk = false;
+		isHLighter = true;
+		isCircle = false;
+		
+	}
+	
+	public void save() {
+		
+		try {
+			
+			ImageIO.write((RenderedImage) image, "png", new File("C:\\MyCanvas\\newImage.png"));
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	public void draganddrop() {
+		
+		initX = 200;
+		initY = 200;
+		radius = 50;
+		
+		ellipse = new Ellipse2D.Double(initX - radius, initY - radius, width, height);
+		g2.setPaint(Color.black);
+		g2.setStroke(new BasicStroke());
+		g2.draw(ellipse);
+		cursor = getCursor();
+		repaint();
+		
+		isDnd = true;
+		isRect = false;
+		pendown = false; 
+		isLine = false;
+		isPoly = false;
+		isFirstClk = true;
+		isHLighter = false;
+		isCircle = false;
+		
+	}
+	
 }
